84e6a5e2c666299fbff18e7248b6e4a2c3dda638
hayasilin <kuanwei.lin@linecorp.com>
2020-01-31 17:55:19 +0800

    First commit

---
+++ ArticleList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArticleList
+//
+//  Created by kuanwei on 2020/1/31.
+//  Copyright © 2020 hayasilin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
